38c1f7626930256f34ba757fc177f0d1ce9505a4
Chandan Bora <chanbora@in.ibm.com>
2016-11-15 15:09:08 +0530

    Added new design by Syed

---
+++ in-progress/resources/js/component.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+$(function(){
+
+	/*$('body').on('click touchstart','.demoComponent',function(){
+		 event.preventDefault();
+		var $this	=	$(this);
+		var url = $this.attr('data-url');
+		
+            $('.modal-body').empty();
+			
+		$.get(url, function(data) {
+		
+			$("#myModal").find('.modal-body').html(data);
+	
+		});
+		
+		
+	});	*/
+	
+});
